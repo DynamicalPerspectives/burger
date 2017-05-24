7c1efefae74c8b4eb3f6963082744cf06840992b
Jeanmarie <jeanmarie@dynamicalperspectives.com>
2017-05-24 05:12:06 -0400

    folder and db creation

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Buffalo", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Chicken", false);
